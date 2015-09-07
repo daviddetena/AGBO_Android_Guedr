dbe3b6619f337596866a993ebf73f1504fc58f8c
David de Tena <daviddetenacalvo@gmail.com>
2015-09-07 22:07:05 +0200

    2 weather systems

--- app/src/main/java/com/daviddetena/guedr/MainActivity.java
+++ app/src/main/java/com/daviddetena/guedr/MainActivity.java
@@ -2,6 +2,9 @@ package com.daviddetena.guedr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.weather_image);
+        Button changeButton = (Button) findViewById(R.id.change_system_button);
+
+        // Clase anónima para el listener del botón
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cambiamos imagen al pulsar el botón
+                imageView.setImageResource(R.drawable.offline_weather2);
+            }
+        });
     }
 }
