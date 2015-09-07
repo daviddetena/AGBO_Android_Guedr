b7520ff37982b0ad53b2c42b49980e6bd0423701
David de Tena <daviddetenacalvo@gmail.com>
2015-09-07 22:56:20 +0200

    2 offline weather systems with toggle

--- app/src/main/java/com/daviddetena/guedr/MainActivity.java
+++ app/src/main/java/com/daviddetena/guedr/MainActivity.java
@@ -3,10 +3,10 @@ package com.daviddetena.guedr;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     private ImageView mImageView = null;
 
@@ -16,24 +16,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mImageView = (ImageView) findViewById(R.id.weather_image);
-        findViewById(R.id.system1_button).setOnClickListener(this);
-        findViewById(R.id.system2_button).setOnClickListener(this);
+        final ToggleButton button = (ToggleButton) findViewById(R.id.change_system_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImageView.setImageResource(button.isChecked() ? R.drawable.offline_weather : R.drawable.offline_weather2);
+            }
+        });
     }
 
-    /**
-     * Comprobamos el botón pulsado y comunicamos nuestro controlador con la vista
-     * @param v
-     */
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.system1_button:
-                mImageView.setImageResource(R.drawable.offline_weather);
-                break;
 
-            case R.id.system2_button:
-                mImageView.setImageResource(R.drawable.offline_weather2);
-                break;
-        }
-    }
 }
